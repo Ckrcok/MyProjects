f7b4f36e8e1ec8ab762c2588aa0f99e50f58cd52
Dor <dorzairi@ymail.com>
2021-02-22 15:21:12 -0500

    Expanded the JS logic for the board clicking

--- Connect Four/functions/function.js
+++ Connect Four/functions/function.js
@@ -5,17 +5,19 @@ score1 = 0;
 timer = 60;
 
 /*----- constants -----*/
-const player1 = document.createElement("img");
-player1.classList.add("img");
-player1.src = "/style/assets/Player1.png";
+//  const player1 = document.createElement("img");
+// player1.classList.add("img");
+// player1.src = "/style/assets/Player1.png";
 
-const player2 = " ";
+const player1 = 1;
+const player2 = -1;
 
+playerTurn = 1;
 // const obj1 = document.createElement("img");
 // obj1.src = "assets/Player1.png";
 
 let boardarray = [
-  [0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
@@ -29,32 +31,39 @@ let boardarray = [
 // const bkacgroundSound = new Audio("background.mp3");
 let obj1 = new Image("/assets/Player1.png");
 const board = document.getElementById("insideBoard");
+
 const buttonExit = document.getElementById("buttonExit");
 const buttonReset = document.getElementById("buttonReset");
 const buttonMute = document.getElementById("buttonMute");
-let tr = document.querySelectorAll("tr");
 
+let tr = document.querySelectorAll("tr");
 let td = document.querySelectorAll("td");
+
 // const obj1 = new image("assets/Player1.png");
 
 /*----- event listeners -----*/
 board.addEventListener("click", click);
+board.addEventListener("click", render);
 buttonExit.addEventListener("click", exit);
 buttonReset.addEventListener("click", reset);
 buttonMute.addEventListener("click", mute);
 /*----- functions -----*/
 
 function click(e) {
-  console.log("Clicked the " + event.target.id);
-  innerHTML = "<img src='/style/assets/Player1.png'>";
+  console.log("Player " + playerTurn + " Clicked the " + event.target.id);
+  let x = event.target.id[0];
+  let y = event.target.id[1];
 
   if (playerTurn == 1) {
+    render(e);
+
+    boardarray[x][y] = playerTurn;
+    playerTurn = -1;
+  } else if (playerTurn == -1) {
+    render(e);
+    boardarray[x][y] = playerTurn;
+    playerTurn = 1;
   }
-
-  let x_from_id = event.target.id[0];
-  let y_from_id = event.target.id[1];
-
-  boardarray[x_from_id][y_from_id] = playerTurn;
 }
 
 function exit(e) {
@@ -112,17 +121,24 @@ for (let i = 0; i < td.length; i++) {
 //   DOMboard.appendChild(row);
 // });
 
-function render() {
-  setTimeout(render, 1000);
-  console.log("Drawing...");
-  // document.getElementById("0").boardarray
-  if (boardarray[i] == 1) {
-    document.getElementById(event.target.id).innerHTML =
+function render(e) {
+  // setTimeout(render, 1000);
+
+  if (playerTurn == 1) {
+    console.log(
+      "Drawing what Player " + playerTurn + " draw at " + event.target.id
+    );
+    document.getElementById(e.target.id).innerHTML =
       "<img class='img' src='/style/assets/Player1.png'>";
-  } else if (boardarray[i][j] == -1) {
-    document.getElementById(event.target.id).innerHTML =
+  } else if (playerTurn == -1) {
+    console.log(
+      "Drawing what Player  " + playerTurn + " draw at " + event.target.id
+    );
+    document.getElementById(e.target.id).innerHTML =
       "<img class='img' src='/style/assets/Player2.png'>";
+  } else {
+    return;
   }
 }
 
-// render();
+render();
